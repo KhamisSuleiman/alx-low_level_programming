1827590a5570428fabe9bc5196628c12b2be94e4
KhamisSuleiman <mwanyere@gmail.com>
2022-06-06 00:00:50 -0700

    done

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	printf("size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of an long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
+	
+	return (0);
+}
