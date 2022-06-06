f540a61a3a2f2619c046e3c0d61aadfe2ae3b8ba
KhamisSuleiman <mwanyere@gmail.com>
2022-06-06 03:50:12 -0700

    d

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main - prints iszes
- * al sizes shown
+ * main - prints sizes
+ * sizes of diff variables
  * Return: 0 on success
  */
 int main(void)
 {
 	printf("size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of an int: %d byte(s\n)", sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
