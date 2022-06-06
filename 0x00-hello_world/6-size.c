9622e7e26051c2fbf22d2125ea694990c410e5da
KhamisSuleiman <mwanyere@gmail.com>
2022-06-06 03:31:32 -0700

    d

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(){
+/**
+ * main - prints iszes
+ * al sizes shown
+ * Return: 0 on success
+int main()
+{
 	printf("size of char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of an long int: %d byte(s)\n", sizeof(long int));
