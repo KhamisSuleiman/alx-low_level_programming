ea4b11fd96350bb5cddefdccc6629e3b8c63a171
KhamisSuleiman <mwanyere@gmail.com>
2022-06-06 02:41:42 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,7 @@
 /**
  *
  *  * main - prints the phrase "Programming is like building a multilingual puzzle
- *
- *   * Return: 0 if exited properly, non-zero otherwise
- *
- *    */
+ */
 int main(void)
 {
 	char str1[] = "\"Programming is like building a multilingual puzzle";
