3a59e740d23b535f38d4d9a23e8de67058c563d7
KhamisSuleiman <mwanyere@gmail.com>
2022-06-06 02:25:21 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(){
-	printf("\"Programming is like building a multilingual puzzle");
+	char str1[]="\"Programming is like building a multilingual puzzle"
+	puts(str1);
 	return (0);
 }
