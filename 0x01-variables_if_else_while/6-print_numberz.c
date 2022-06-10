afa55631799617154afcef4f3fbbcf91332eb8d6
KhamisSuleiman <mwanyere@gmail.com>
2022-06-09 22:49:53 -0700

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints all numbers
+ * from 0 - 9 using putchar
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 20; n < 30; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
